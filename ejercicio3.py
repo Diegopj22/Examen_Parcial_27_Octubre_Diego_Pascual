6b984fc95180c569f73b0b47ba9fd266713c860a
Diegopj22 <dpj2202@gmail.com>
2022-10-27 19:34:38 +0200

    prueba 1

--- ejercicio3.py
+++ ejercicio3.py
@@ -2,5 +2,9 @@ class Alumno:
     def __init__(self,nombre,nota):
         self.nombre=nombre
         self.nota=nota
+
+    def __str__(self):
+        return """
         
+        """   
         
