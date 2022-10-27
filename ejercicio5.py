f8e2dd84ab4a90d946650af31e744873723797ac
Diegopj22 <dpj2202@gmail.com>
2022-10-27 20:49:24 +0200

    determinante

--- ejercicio5.py
+++ ejercicio5.py
@@ -1,1 +1,13 @@
-import numpy as pd
+import numpy as np
+
+def main():
+  
+  m = np.array([(1, 2, 3), (2, 5, 2), (1, 3, 1),(2,3,2),(4,1,2)])
+   
+  # calcula el determinante usando la regla de Sarrus
+  det = ((m[0][0] * m[1][1] * m[2][2]) + (m[0][1]  
+    * m[1][2] * m[2][0]) + (m[0][2] * m[1][0] * m[2][1])) - ((m[2][0] 
+    * m[1][1] * m[0][2]) + (m[2][1]  * m[1][2] * m[0][0]) + (m[2][2] 
+    * m[1][0] * m[0][1]))
+     
+  
