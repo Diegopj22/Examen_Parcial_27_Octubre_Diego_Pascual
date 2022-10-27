7c9f44d65556c6f747e4f13cec3efef595765b3c
Diegopj22 <dpj2202@gmail.com>
2022-10-27 19:56:47 +0200

    commit 2

---
+++ ejercicio3/codigo.py
@@ -0,0 +1,17 @@
+class Alumno:
+    def __init__(self,nombre,nota):
+        self.nombre=nombre
+        self.nota=nota
+
+    def __str__(self):
+         return(self.nombre,self.nota)
+    
+    
+    def calificacion (self):
+        if self.nota < 5 :
+            print("Suspenso")
+        else:
+            print("Aprobado") 
+
+diego=Alumno("diego",2)
+diego.calificacion()        
