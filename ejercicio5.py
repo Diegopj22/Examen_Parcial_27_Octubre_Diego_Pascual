cad6e4c9d2924a9a19dd0a36c7cd2145139badca
Diegopj22 <dpj2202@gmail.com>
2022-10-27 20:55:48 +0200

    me sale

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,13 +2,10 @@ import numpy as np
 
 def main():
   
-  m = np.array([(1, 2, 3), (2, 5, 2), (1, 3, 1),(2,3,2),(4,1,2)])
+  m = np.diag([1,2,3,4,5])
    
   # calcula el determinante usando la regla de Sarrus
-  det = ((m[0][0] * m[1][1] * m[2][2]) + (m[0][1]  
-    * m[1][2] * m[2][0]) + (m[0][2] * m[1][0] * m[2][1])) - ((m[2][0] 
-    * m[1][1] * m[0][2]) + (m[2][1]  * m[1][2] * m[0][0]) + (m[2][2] 
-    * m[1][0] * m[0][1]))
+  det = np.linalg.det(m)
 
   print("El determinante de la matriz es: %f" % det)
    
