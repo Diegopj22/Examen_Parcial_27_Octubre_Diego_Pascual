3c5c249a2c53b78c4241b3031e834dcb933d9bae
Diegopj22 <dpj2202@gmail.com>
2022-10-27 20:14:44 +0200

    commit

--- ejercicio4/main.py
+++ ejercicio4/main.py
@@ -2,7 +2,7 @@ from codigo2 import Alumno2:
 
 if __name__=='__main__':
 
-    pedro=Alumno("pedro",4)
-    carlos=Alumno("carlos",2,45,160)
+    print(pedro=Alumno2("pedro",4))
+    print(carlos=Alumno2("carlos",2,45,160))
 
     
