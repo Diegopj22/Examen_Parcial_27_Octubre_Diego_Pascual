c20b72fb71fa0e8d38ad8dac369cc962df37fa82
Diegopj22 <dpj2202@gmail.com>
2022-10-27 20:05:05 +0200

    copia ejercicio3

---
+++ ejercicio4.py
@@ -0,0 +1,15 @@
+class Alumno:
+    def __init__(self,nombre,nota):
+        self.nombre=nombre
+        self.nota=nota
+
+    def __str__(self):
+         return(self.nombre,self.nota)
+    
+    
+    def calificacion (self):
+        if self.nota < 5 :
+            print("Suspenso")
+        else:
+            print("Aprobado") 
+    
